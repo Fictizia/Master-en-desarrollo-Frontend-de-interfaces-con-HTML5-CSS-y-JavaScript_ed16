340139a2a4c263b6efcf158f6db14e14689de4d8
Web <web@ZiMac2-08.local>
2020-01-29 21:10:07 +0100

    pesudocodigo

---
+++ Jose_Jesus/pseudocodigo.js
@@ -0,0 +1,35 @@
+/*
+
+1- Diseña un programa que lea los dos números y realice los siguientes cálculos
+
+valor de su suma
+valor de su resta
+valor de su division
+valor de su producto
+
+   Escribir "El primer numero:"
+	Leer valor1
+	
+	Escribir "El segundo numero:"
+    Leer valor2
+    
+    // suma
+    resultado <- valor1+valor2
+    Escribir "el valor de la suma es", resultado
+
+     // resta
+    resultado <- valor1-valor2
+    Escribir "el valor de la resta es", resultado
+
+    // division
+    resultado <- valor1/valor2
+    Escribir "el valor de la division es", resultado
+
+    // producto
+    resultado <- valor1*valor2
+    Escribir "el valor del producto es", resultado
+	
+
+
+*/
+
