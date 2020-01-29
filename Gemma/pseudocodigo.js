de8927ef99596586e790a72f90f8cd8ab028d4ac
Web <web@ZiMac2-03.local>
2020-01-29 21:09:33 +0100

    Programa numeros

---
+++ Gemma/pseudocodigo.js
@@ -0,0 +1,22 @@
+/*
+    Valor de su suma
+    Valor de su resta
+    Valor de su division
+    Valor de su producto
+
+    var a = 8
+    var b = 2
+
+    suma = (a+b)
+        Escribir 'El resultado es' suma
+
+    resta = (a-b)
+        Escribir 'El resultado es' resta
+
+    division = (a/b)
+        Escribir 'El resultado es' division
+
+    producto = (a*b)
+        Escribir 'El resultado es' producto
+
+*/
