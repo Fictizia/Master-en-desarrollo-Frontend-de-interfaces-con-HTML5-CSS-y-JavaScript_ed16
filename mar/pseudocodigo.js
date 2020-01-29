84113f7a25ecec329864053afc2b2676269b3cfa
Web <web@ZiMac2-05.local>
2020-01-29 21:10:08 +0100

    mi primera practica mar

---
+++ mar/pseudocodigo.js
@@ -0,0 +1,38 @@
+/*  
+
+Diseña un programa que lea dos números y realice los siguientes cálculos:
+
+Valor de su suma
+Valor de su resta
+Valor de su division
+Valor de su producto
+
+Proceso de calculo
+
+Valor del primer número:
+    Escribir número 8
+    Leer número 8
+
+Valor del segundo número:
+    Escribir número 2
+    Leer número 2
+
+//Summa:
+    resultado = 8+2
+    Escribir "El resultado es", 10
+
+//Resta:
+    resultado = 8-2
+    Escribir "El resultado es", 6
+
+//Multiplicación:
+    resultado = 8*2
+    Escribir "El resultado es", 16
+
+//División:
+    resultado = 8/2
+    Escribir "El resultado es", 4
+
+Fin del proceso
+
+*/
