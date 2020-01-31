e424e80b4e89d58b2a20e8b834564258bb38e1c3
jjzamorasierra <josejesuszamora@gmail.com>
2020-01-31 21:51:20 +0100

    dia3_ejercicio

---
+++ Jose_Jesus/clase3/dia3_ejercicio.js
@@ -0,0 +1,11 @@
+/* Tarea 1
+ - Declara una variable que contenga tu nombre
+ - ¡mprimir por pantalla saludandote amablemente "Hola" [Nombre]
+ 
+Tarea 2
+- Declara una variable de tipo númerico inicializada con valor 100
+- Decalra una segunda variable inicializada con el valor que desees
+- Haz una operaciómn de tipo +, -, *, /,  y % ( imprimir por consola resultados)
+- Juega con los operadores ++, --, +=, -=,
+*/
+
