a2190bc6cc9e3ac56a15458ad80f13165055cdb0
Web <web@ZiMac2-07.local>
2020-01-31 21:47:24 +0100

    Ejercicio clase 3

---
+++ Kiril/Clase3/tareas_dia_3.js
@@ -0,0 +1,16 @@
+/*
+Tarea 1
+    - Declara una variable que contenga tu nombre
+    - Imprimir por pantalla saludandote amablemente 'Hola [Nombre]'
+
+Tarea 2
+    - Declara una variable de tipo Numérico inicializada con valor 100
+    - Declara una segunda variable y inicializala con el valor que desees
+    - Haz unas operaciones de tipo: + , - , * , / , % (imprimir por consola resultado)
+    - Juega con los operadores ++ , -- , += , -= , 
+*/
+
+var nombre = "Kiril";
+console.log = "Hola ", nombre;
+
+var num1
