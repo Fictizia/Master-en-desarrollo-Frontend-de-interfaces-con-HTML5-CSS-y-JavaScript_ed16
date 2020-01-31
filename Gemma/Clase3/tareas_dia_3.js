8ba8c6fa31b5281d4e20781711c897b6467398ce
Web <web@ZiMac2-03.local>
2020-01-31 21:47:29 +0100

    ejercicios clase 3

---
+++ Gemma/Clase3/tareas_dia_3.js
@@ -0,0 +1,13 @@
+/*
+Tarea 1
+    - Declara una variable que contenga tu nombre
+    - Imprimir por pantalla saludandote amablemente 'Hola [Nombre]'
+
+Tarea 2
+    - Declara una variable de tipo numérico iniciala con valor 100
+    - Declara una segunda variable e iniciala con el valor que desees
+    - Haz unas operaciones de tipo +, -, *, / y % (imprimir consola resultados)
+    - Juega con los operadores ++, --, +=, -=
+*/
+
+var 
