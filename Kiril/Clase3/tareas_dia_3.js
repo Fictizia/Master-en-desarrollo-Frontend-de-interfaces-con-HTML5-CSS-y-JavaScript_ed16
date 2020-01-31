5e1bbdd1e4cdc0416998db60b8908da4bf9f1ba5
Web <web@ZiMac2-07.local>
2020-01-31 21:57:17 +0100

    tarea1 resuelta

--- Kiril/Clase3/tareas_dia_3.js
+++ Kiril/Clase3/tareas_dia_3.js
@@ -13,4 +13,13 @@ Tarea 2
 var nombre = "Kiril";
 console.log = "Hola ", nombre;
 
-var num1
+var num1 = 100;
+var num2 = 200; 
+suma = num1 + num2;
+resta = num1 - num2;
+multiplicacion = num1 * num2;
+division = num1 / num2;
+resto = num1 % num2; 
+
+console.log = "La suma de ", num1, "+ " , num2 , "es igual a: ", suma;
+console.log = "La suma es: " , suma;
