de60480b3ba6881cfee388481040856196b61bf8
FranciscoValdesoiro <franciscovaldesoirogil@gmail.com>
2020-01-31 21:47:22 +0100

    añadidos ejercicios clase 3

---
+++ Fran/clase3/tareas_dia_3.js
@@ -0,0 +1,11 @@
+/*
+ Tarea 1
+  - Declara una variable que contenga tu nombre
+  - Imprimir por pantalla saludandote amablemente 'Hola [Nombre]'
+
+ Tarea 2
+  - Declara una variable de tipo numérico inicializala con valor 100
+  - Declara una segunda variable y inicializala con el valor que desees
+  - Haz una operaciónes de tipo +, -, *, / y % (imprimir por consola resultados)
+  - Juega con los operadores ++, --, +=, -=.
+*/
