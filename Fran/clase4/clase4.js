971438e7aacd47b375edde2a97160437e6de1cc0
FranciscoValdesoiro <franciscovaldesoirogil@gmail.com>
2020-02-03 22:04:10 +0100

    add input task

---
+++ Fran/clase4/clase4.js
@@ -0,0 +1,28 @@
+/*
+    Ejercicios Clase 4
+
+    1.  - Preguntar el nombre al usuario y guardarlo en una variable
+        - Imprimir por consola un saludo agradable al usuario con su
+          nombre. 
+
+          function calcularSuma(num1, num2){
+              console.log("El número 1 es: " + num1)
+              console.log("El número 2 es: " + num2)
+              console.log("El resultado de la suma es: " + (num1 + num2))
+          }
+
+          - nombre por prompt
+          - guardar en variable nombre
+          - pasarselo a la funcion saludar
+
+          var nombre = pro.......
+
+           function saludar(nombre){
+              console.log("Hola " 
+              );
+          }
+
+          saludar(nombre)
+
+*/
+
