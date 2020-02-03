d046ede05187bcd810f876b5eb9a2c08f74c121c
María del Mar <mariadelmar@MacBook-Pro-de-Fernando.local>
2020-02-03 11:27:08 +0100

    añadir tarea dia 3

---
+++ mar/clase3Mar/practica3Mar.js
@@ -0,0 +1,84 @@
+/*
+Tarea 1
+    - Declara una variable que contenga tu nombre
+    - Imprimir por pantalla saludandote amablemente 'Hola [Nombre]'
+
+    Tarea 2
+    - Declara una variable de tipo Numérico inicializada con valor 100
+    - Declara una segunda variable e inicializala con el valor que desees
+    - Haz unas operaciones de tipo: + , - , * , / , % (imprimir por consola resultado)
+    - Juega con los operadores ++ , -- , += , -= , 
+*/
+
+/*
+Tarea 1
+var nombre = "Hola María del Mar";
+console.log("%c %s", "background: #24a4ad; color: #FFF; font-size:20px;" ,nombre);
+//Respuesta de la consola ==>  Hola María del Mar
+
+
+Tarea 2
+var x = 1001;
+var y = 10;
+
+Suma:
+suma = x + y
+//Resultado consola ==> 1011
+
+Resta:
+resta = x-y
+//Resultado consola ==> 991
+
+Multiplicación:
+multiplicación = x * y
+//Resultado consola ==> 10010
+
+División:
+división = x / y
+//Resultado consola ==> 100.1
+
+Resto:
+resto = x % y
+//Resultado consola ==> 1
+
+CRECIMIENTO Y DECRECIMIENTO
+
+var numero = 88;
+console.log (--numero); //87
+
+var numero = 88;
+console.log (numero--); //88
+console.log (numero--); //87
+
+var numero = 88;
+console.log(++numero); //89
+console.log (++numero); //90
+
+var numero = 88;
+console.log(numero++); //88
+console.log(numero++); //89
+
+OPERADORES DE ASIGNACIÓN
+
+= Asignacion
+var x = 1000;
+var y = 10;
+x = y;
+console.info("\"x\" vale ", x);
+//Resultado consola ==> "x" vale  10
+
++= Suma
+var x = 1000;
+var y = 10;
+x += y;
+console.info("\"x\" vale ", x);
+//Resultado consola ==>"x" vale  1010
+
+-= Resta
+var x = 1000;
+var y = 10;
+x -= y;
+console.info("\"x\" vale ", x);
+//Resultado consola ==>"x" vale  990
+
+*/
