8bd2b246c5e787f13ae3087366fe008b3b2e47b5
mdmperez <mdmperez91@gmail.com>
2020-02-03 22:03:55 +0100

    add clase4

---
+++ mar/clase4/clase4.js
@@ -0,0 +1,36 @@
+/*
+    Ejercicio Clase 4
+
+    1. - Preguntar el nombre al usuario y guardarlo en una variable
+       - Imprimir por consola un saludo agradable al usuario con su nombre
+          
+       function calcularSuma(num1, num2) {
+        console.log ("el número 1 es: "+  num1);   
+        console.log ("el número 2 es: "+  num2);  
+        console.log("el resultado de la suma es: " + (num1 + num2));
+       }
+
+    2. - Nombre por prompt
+       - Guardar en variable nombre
+       - Pasarlo a la función saludar
+
+//Ejercicio 1
+
+var nombre = prompt("¿Como te llamas?"); //Mar
+console.log("Hola "+usuario+", ¿qué tal?"); //Hola Mar, ¿qué tal?
+
+calcularSuma(2,3)
+el número 1 es: 2
+el número 2 es: 3
+el resultado de la suma es: 5
+
+//Ejercicio 2
+
+var nombre = prompt("¿Como te llamas?");
+function saludar(nombre){
+console.log("Hola " + nombre)
+}
+
+saludar(nombre)
+
+*/
