bb511451f088936d855555a7e9f1b48350949dfc
Pepovkp <kiko.pepo@gmail.com>
2020-02-07 21:57:56 +0100

    Ejercicio

--- Kiril/clase6/numeros.js
+++ Kiril/clase6/numeros.js
@@ -1,1 +1,14 @@
-// test
+/* Diseña un algoritmo que simula el lanzamiento de una moneda al aire e imprimir si ha salido cara o cruz.
+
+*/
+
+function lanzarmoneda (){
+    var numero = Math.floor((Math.random() * 2) + 1);
+        if (numero === 1){
+            console.log("Ha salido cara");
+        }
+            else{
+            console.log("Ha salido cruz");
+        }
+        
+}
