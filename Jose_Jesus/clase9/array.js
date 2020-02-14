3b56e5341d7b5c888db26cc8024e440d71c9be07
jjzamorasierra <josejesuszamora@gmail.com>
2020-02-14 22:05:11 +0100

    clase9

---
+++ Jose_Jesus/clase9/array.js
@@ -0,0 +1,31 @@
+console.log ('loaded');
+
+
+/*
+
+Array
+
+Ejercicio 1:
+
+Crear un array que contenga el nombre de 5 amigos, mascotas o frutas y después recorre ese array e imprimelo por pantalla (con for, map o foreach)
+
+*/
+
+
+
+var amigos= ["Juan","Dani","Sara","Iñigo"];
+var resultado = amigos.map(function (elemento)
+{return elemento + " es mi amigo/a "});
+console.log(resultado);
+
+var lista = document.getElementById("lista")
+
+function addElementInlist(list){
+    list.forEach(element =>{
+        var li=document.createElement("li");
+        li.appendChild(document.createTextNode(element));
+        lista.appendChild(li);
+    })
+}
+
+addElementInlist(amigos)
