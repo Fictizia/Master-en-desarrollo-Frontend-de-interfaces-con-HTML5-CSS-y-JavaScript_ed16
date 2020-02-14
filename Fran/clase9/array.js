38b5e0ab82be7b8d64cc74d7869d2faf07480839
FranciscoValdesoiro <franciscovaldesoirogil@gmail.com>
2020-02-14 21:33:50 +0100

    add title

--- Fran/clase9/array.js
+++ Fran/clase9/array.js
@@ -1,1 +1,9 @@
 console.log('loaded');
+
+/*
+    Array
+
+    Ejercicio 1:
+    Crea un array que contenga el nombre de 5 amigos, mascotas o frutas y despues recorre
+    ese array e imprimelo por pantalla (con for, map o forEach) 
+*/
