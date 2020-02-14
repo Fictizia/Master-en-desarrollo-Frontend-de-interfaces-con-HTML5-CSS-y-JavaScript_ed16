957f9b92bd796168ace3d910fdd6a5e905f82aa7
FranciscoValdesoiro <franciscovaldesoirogil@gmail.com>
2020-02-14 22:06:54 +0100

    changes

--- Fran/clase9/array.js
+++ Fran/clase9/array.js
@@ -7,3 +7,17 @@ console.log('loaded');
     Crea un array que contenga el nombre de 5 amigos, mascotas o frutas y despues recorre
     ese array e imprimelo por pantalla (con for, map o forEach) 
 */
+
+var animals = ["Gato", "Perro", "Elefante", "Iguana", "Mapache"]
+
+var lista = document.getElementById("lista")
+
+function addElementInList(list){
+    list.forEach(element => {
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(element));
+        lista.appendChild(li);    
+    });
+}
+
+addElementInList(animals)
