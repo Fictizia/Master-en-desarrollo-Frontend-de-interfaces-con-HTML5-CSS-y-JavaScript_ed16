8b8fc8ac6222767aaea3f1321f3fbc0ae475a387
mdmperez <mdmperez91@gmail.com>
2020-02-21 22:02:37 +0100

    add ejercicios de hoy

--- mar/clase11/purpose.js
+++ mar/clase11/purpose.js
@@ -1,12 +1,38 @@
 var list1 = ['Ir al gimnasio', 'Mejorar mi inglés', 'Viajar a Miami', 'Dejar de fumar', 'Cambiar de trabajo', 'Ir a más conciertos'];
-var list2 = ['Ir al gimnasio', 'Dejar de fumar'];
+var list2 = ['Dejar de fumar'];
 
-let ul = document.getElementById('list1');
+let ulpurpose = document.getElementById('list1');
+let ulCheck = document.getElementById('list2');
 
-list1.forEach(item => {
-    let li = document.createElement("li");
-    li.innerHTML = item;
-    ul.appendChild(li);
+function cambiarFondo() {
+    var color = 'rgb(' + Math.floor((Math.random() * 255))+ ',';
+    color += Math.floor((Math.random() * 255)) + ',';
+    color += Math.floor((Math.random() * 255)) + ')';
+    document.body.style.backgroundColor= color;
+    console.info("Nuevo color:", color);
+}
+
+refrescarListas();
+
+function refrescarListas(){
+    list1.forEach(item => {
+        let li = document.createElement("li");
+        li.innerHTML = item;
+        li.setAttribute("class", "red");
+        ulpurpose.appendChild(li);
+    })
+    
+    list2.forEach(item => {
+        let li = document.createElement("li");
+        li.innerHTML = item;
+        li.setAttribute("class", "green");
+        ulCheck.appendChild(li);
+    })
+}
+
+ulpurpose.addEventListener('click', function (e) {
+    console.log("Target: ", e.target);
 })
 
 
+//truco: push, inner text, index off,  splice 
