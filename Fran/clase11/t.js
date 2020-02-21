ab284bcd141d3f3bf96b4c60028f401233e906fc
FranciscoValdesoiro <franciscovaldesoirogil@gmail.com>
2020-02-21 22:05:40 +0100

    changes

--- Fran/clase11/t.js
+++ Fran/clase11/t.js
@@ -7,8 +7,8 @@ const lista2Element = document.getElementById("lista2");
 
 refrescarListas()
 
-lista1Element.addEventListener("click", (e) =>
-    console.log("target: ",e.target, " currentTarget: ", e.currentTarget))
+const titleHTMLElement = document.getElementById("title")
+
 
 lista1Element.addEventListener("click", (e) => {
     lista2.push(e.target.innerText)
@@ -18,25 +18,33 @@ lista1Element.addEventListener("click", (e) => {
 
 lista2Element.addEventListener("click", (e) => {
     lista1.push(e.target.innerText)
-    lista2.splice(lista2.indexOf(e.target.innerText), 1)
+    var index = lista2.indexOf(e.target.innerText)
+    lista2.splice(index, 1)
     refrescarListas()
 })
 
-
-
+ function cambiarFondo() {
+		// color = 'rgb(0-255,0-255,0-255'
+		var color = 'rgb(' + Math.floor((Math.random() * 255))+ ',';
+		color += Math.floor((Math.random() * 255)) + ',';
+		color += Math.floor((Math.random() * 255)) + ')';
+		document.body.style.backgroundColor= color;
+		console.info("Nuevo color:", color);
+}
 
 function refrescarListas(){
-    lista1Element.innerHTML = ''
-    lista2Element.innerHTML = ''
     lista1.forEach(item => {
         let li = document.createElement("li");
         li.innerHTML = item;
+        li.setAttribute("class", "red")
         lista1Element.appendChild(li);
     })
     
     lista2.forEach(item => {
         let li = document.createElement("li");
         li.innerHTML = item;
+        li.setAttribute("class", "green")
         lista2Element.appendChild(li);
     })
-}
+} 
+
