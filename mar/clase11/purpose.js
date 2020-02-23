278edd2eff9455db0155d8f19d028b53aa53cd02
María del Mar <mdmperez91@gmail.com>
2020-02-23 13:55:30 +0100

    add tareas dia 12

--- mar/clase11/purpose.js
+++ mar/clase11/purpose.js
@@ -1,5 +1,5 @@
 var list1 = ['Ir al gimnasio', 'Mejorar mi inglés', 'Viajar a Miami', 'Dejar de fumar', 'Cambiar de trabajo', 'Ir a más conciertos'];
-var list2 = ['Dejar de fumar'];
+var list2 = [];
 
 let ulpurpose = document.getElementById('list1');
 let ulCheck = document.getElementById('list2');
@@ -15,6 +15,7 @@ function cambiarFondo() {
 refrescarListas();
 
 function refrescarListas(){
+    ulpurpose.innerHTML = "";
     list1.forEach(item => {
         let li = document.createElement("li");
         li.innerHTML = item;
@@ -22,6 +23,7 @@ function refrescarListas(){
         ulpurpose.appendChild(li);
     })
     
+    ulCheck.innerHTML ="";
     list2.forEach(item => {
         let li = document.createElement("li");
         li.innerHTML = item;
@@ -31,8 +33,21 @@ function refrescarListas(){
 }
 
 ulpurpose.addEventListener('click', function (e) {
-    console.log("Target: ", e.target);
+    var itemSelected = e.target.innerText;
+    list2.push(itemSelected);
+    var itemClick = list1.indexOf(itemSelected);
+    list1.splice(itemClick,1);
+    refrescarListas();
 })
+
+ulCheck.addEventListener('click', function (e) {
+    var itemSelected = e.target.innerText;
+    list1.push(itemSelected);
+    var itemClick = list2.indexOf(itemSelected);
+    list2.splice(itemClick,1);
+    refrescarListas();
+})
+
 
 
 //truco: push, inner text, index off,  splice 
