f10647a8b2aa914e70e9e73b82cec6df2b5d596d
Web <web@ZiMac2-03.local>
2020-02-24 20:01:40 +0100

    ejercico lista compra terminado

--- Gemma/clase 11/listacompra.js
+++ Gemma/clase 11/listacompra.js
@@ -1,4 +1,5 @@
 var listaFrutas = ['Plátanos', 'Uvas', 'Kiwis', 'Cerezas', 'Fresas', 'Manzanas'];
+var listaComida = ['Leche', 'Huevos', 'Pescado', 'Queso', 'Pan'];
 
 
 let ul = document.getElementById ('lista1');
@@ -11,9 +12,16 @@ listaFrutas.forEach(item => {
     
 });
 
-ul.addEventListener('click', e => console.log(e.target));
-
-var listaComida = ['Leche', 'Huevos', 'Pescado', 'Queso', 'Pan'];
+ul.addEventListener('click', e => {
+    console.log(e.target.innerHTML);
+    
+    listaComida.push(e.target.innerText);
+    
+    var index = listaFrutas.indexOf(e.target.innerText);
+    listaFrutas.splice(index,1);
+    
+    refrescarListas()
+});
 
 let ul2 = document.getElementById ('lista2');
 
@@ -24,8 +32,22 @@ listaComida.forEach(item => {
     ul2.appendChild(li);
 });
 
-function refrescarListas(){
+
+ul2.addEventListener('click', e => {
+    console.log(e.target.innerHTML);
     
+    listaFrutas.push(e.target.innerText);
+    
+    var index = listaComida.indexOf(event.target.innerText);
+    listaComida.splice(index,1);
+    
+    refrescarListas()
+});
+
+
+function refrescarListas(){
+    ul.innerHTML = '';
+    ul2.innerHTML = '';
     listaFrutas.forEach(item => {
         let li = document.createElement('li');
         li.innerHTML = item;
@@ -47,16 +69,22 @@ function refrescarListas(){
 
 
 
-const titleElement = document.getElementById ('title');
-titleElement.addEventListener ('click', e => {
-    console.log(`target${e.target}`);
-    console.log(`currentTarget${e.currentTarget}`); 
-}
-)
+// const titleElement = document.getElementById ('lista1');
+// titleElement.addEventListener ('click', e => {
+//     console.log(`target ${e.target.innerText}`);
+//     console.log(`currentTarget ${e.currentTarget}`);
+//     listaFrutas.push();
+
+//     var index = listaComida.indexOf
+//     listaComida.splice(index, 1);
+
+    
+
+// }
+// )
 
 //splice & indexOf innerText
 /*
-var lista 1 = ['banana', 'cebolla', 'perejil', 'pimientos']
 lista1.push()
 var index = lista2.indexOf(e.target.innerTExt)
 lista2.splice(index, 1)
