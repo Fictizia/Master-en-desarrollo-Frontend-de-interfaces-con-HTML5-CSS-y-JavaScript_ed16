b10d73e46f460c0506d846cf701059d6684a9ac5
mdmperez <mdmperez91@gmail.com>
2020-02-28 21:59:18 +0100

    add

---
+++ mar/clase15/js.js
@@ -0,0 +1,17 @@
+fetch("https://pokeapi.co/api/v2/pokemon")
+        .then( response => response.json() )
+            .then( result => insertarElems(result.results) )
+
+const miLista = document.getElementById('list_pokemon');
+const miLista2 = document.getElementById('list_pokemon-like');
+
+function insertarElems(results) {
+    miLista.innerHTML = "";
+    results.forEach(item => {
+        let li = document.createElement("li");
+        li.innerText = item.name;
+        li.setAttribute("class", "list_item");
+        miLista.appendChild(li);
+    })
+  
+}
